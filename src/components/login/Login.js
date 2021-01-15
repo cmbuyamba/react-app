e6daddc6814efc17fc2b76920431984e339b041e
Celestin Mbuyamba <celembuyamba@gmail.com>
2021-01-15 00:37:54 -0500

    Added css from Login.css

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,42 +1,27 @@
 import React from 'react';
-import { Button, Container, Avatar, Typography, TextField, FormControlLabel, Checkbox, Grid, Link } from '@material-ui/core';
+import { 
+    Button, 
+    Container, 
+    Avatar, 
+    Typography, 
+    TextField, 
+    FormControlLabel, 
+    Checkbox, 
+    Grid, 
+    Link
+ } from '@material-ui/core';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
-import { makeStyles } from '@material-ui/core/styles';
-import CssBaseline from '@material-ui/core/CssBaseline';
-
-const useStyles = makeStyles((theme) => ({
-    container: {
-      marginTop: theme.spacing(8),
-      display: 'flex',
-      flexDirection: 'column',
-      alignItems: 'center',
-    },
-    avatar: {
-      margin: theme.spacing(1),
-      backgroundColor: theme.palette.secondary.main,
-    },
-    form: {
-      width: '100%', // Fix IE 11 issue.
-      marginTop: theme.spacing(1),
-    },
-    submit: {
-      margin: theme.spacing(3, 0, 2),
-    },
-  }));
+import './Login.css';
 
 const Login = () => {
-
-    const classes = useStyles();
-
     return (
         <Container component="main" maxWidth="xs">
-            <CssBaseline />
-            <div className={classes.container}>
-                <Avatar className={classes.avatar}>
+            <div className="container">
+                <Avatar className="avatar">
                     <LockOutlinedIcon />
                 </Avatar>
                 <Typography component="h1" variant="h5">Sign In</Typography>
-                <form className={classes.form}>
+                <form className="form">
                     <TextField variant="outlined"
                         required
                         label="Enter Email"
@@ -44,7 +29,7 @@ const Login = () => {
                         name="email"
                         id="email"
                         type="email"
-                        fullWidth={true}
+                        fullWidth
                     />
                     <TextField variant="outlined"
                         required
@@ -63,7 +48,7 @@ const Login = () => {
                         type="submit"
                         fullWidth
                         color="primary"
-                        className={classes.submit}
+                        className="submit"
                         variant="contained"
                     >
                         SIGN IN
