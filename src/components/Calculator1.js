7f8a4d952f23fc3cd7c000e313365b4e78f9e9e7
Celestin Mbuyamba <celembuyamba@gmail.com>
2021-01-16 15:48:24 -0500

    Added prettier config

--- src/components/Calculator1.js
+++ src/components/Calculator1.js
@@ -1,49 +1,56 @@
-import React, { useState } from 'react';
-import Header from './Header';
-import calculator from '../services/calculator';
-import Input from './common/input/Input';
+import React, { useState } from "react";
+import Header from "./Header";
+import calculator from "../services/calculator";
+import Input from "./common/input/Input";
 
 //Hooks
 
 const Calculator1 = () => {
+  const [number1, setNumber1] = useState(0);
+  const [number2, setNumber2] = useState(0);
+  const [result, setResult] = useState(0);
 
-const [number1, setNumber1] = useState(0);
-const [number2, setNumber2] = useState(0);
-const [result, setResult] = useState(0);
+  const onChange = (event) => {
+    if (event.target.name === "number1") {
+      setNumber1(event.target.value);
+    } else {
+      setNumber2(event.target.value);
+    }
+  };
 
-    const onChange = (event) => {
-        if(event.target.name === "number1") {
-            setNumber1(event.target.value);
-        }
-        else {
-            setNumber2(event.target.value);
-        }
-    };
+  const onClick = (event) => {
+    const operation = event.target.innerText;
+    if (operation === "Add") {
+      setResult(calculator.add(number1, number2));
+    } else if (operation === "Multiply") {
+      setResult(calculator.multiply(number1, number2));
+    } else {
+      setResult(calculator.divise(number1, number2));
+    }
+  };
 
-    const onClick = (event) => {
-        const operation = event.target.innerText;
-        if(operation === "Add"){
-            setResult(calculator.add(number1, number2));
-            
-        }
-        else if (operation === "Multiply") {
-            setResult(calculator.multiply(number1, number2));
-        }  else {
-            setResult(calculator.divise(number1, number2));
-        }
-    };
-
-        return <div>
-            <Header title={'My Calculator'} />
-            <Input onChange={onChange} label="Enter first number 1" name="number1" value={number1}/>
-            <Input onChange={onChange} label="Enter second number 2" name="number2" value={number2}/>
-            <label>{result}</label>
-            <br/>
-            <button onClick={onClick}>Add</button>
-            <button onClick={onClick}>Multiply</button>
-            <button onClick={onClick}>divise</button>
-        </div>;
-    
+  return (
+    <div>
+      <Header title={"My Calculator"} />
+      <Input
+        onChange={onChange}
+        label="Enter first number 1"
+        name="number1"
+        value={number1}
+      />
+      <Input
+        onChange={onChange}
+        label="Enter second number 2"
+        name="number2"
+        value={number2}
+      />
+      <label>{result}</label>
+      <br />
+      <button onClick={onClick}>Add</button>
+      <button onClick={onClick}>Multiply</button>
+      <button onClick={onClick}>divise</button>
+    </div>
+  );
 };
 
 export default Calculator1;
--- src/components/common/button/Button.js
+++ src/components/common/button/Button.js
@@ -1,11 +1,12 @@
-import React from 'react';
-import './Button.css';
+import React from "react";
+import "./Button.css";
 
-const Button = ({label, onClick, className}) => {
-    return(
-    <button className={className} onClick={onClick}>{label}</button>
-    );
+const Button = ({ label, onClick, className }) => {
+  return (
+    <button className={className} onClick={onClick}>
+      {label}
+    </button>
+  );
 };
-
 
 export default Button;
